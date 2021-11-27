0e13ce777c9524eeb03a6b93079812fb515157ec
Vitaly Turovsky <vital2580@icloud.com>
2021-11-27 14:18:15 +0300

    feat: add prettier task

---
+++ prettier/index.ts
@@ -0,0 +1,15 @@
+import { packageJson } from 'mrm-core'
+
+module.exports = () => {
+    packageJson()
+        .set('prettier', {
+            semi: false,
+            singleQuote: true,
+            proseWrap: 'never',
+            tabWidth: 4,
+            trailingComma: 'all',
+            arrowParens: 'avoid',
+            printWidth: 150,
+        })
+        .save()
+}
