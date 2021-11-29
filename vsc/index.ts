bad011fe49d9614d0935b8b3d10837e48eea7597
Vitaly Turovsky <vital2580@icloud.com>
2021-11-29 11:20:52 +0300

    feat: new preset vsc

---
+++ vsc/index.ts
@@ -0,0 +1,37 @@
+import { file, install, packageJson } from 'mrm-core'
+import { basename } from 'path'
+import { PackageJson } from 'type-fest'
+import fsExtra from 'fs-extra'
+
+/** New VSCode Extension */
+module.exports = () => {
+    if (packageJson().exists()) {
+        console.warn('package.json already exists')
+        return
+    }
+
+    file('package.json').save(
+        JSON.stringify(
+            {
+                name: basename(process.cwd()),
+                version: '0.0.0-dev',
+                // have no idea how to retrive name like that
+                author: 'zardoy',
+                categories: [],
+                contributors: {} as any,
+                license: 'MIT',
+                scripts: {
+                    start: 'vscode-framework start',
+                },
+                dependencies: {},
+                devDependencies: {},
+            } as PackageJson,
+            undefined,
+            4,
+        ),
+    )
+    install(['vscode-framework'], {
+        pnpm: true,
+    })
+    fsExtra.ensureFileSync(`src/extension.ts`)
+}
