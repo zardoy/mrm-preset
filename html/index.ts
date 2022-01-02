a1a4151c8737911636e3c97770efad8745a8200d
Vitaly Turovsky <vital2580@icloud.com>
2022-01-02 08:56:21 +0300

    chore: package cleanup feat: add draft tailwind task

--- html/index.ts
+++ html/index.ts
@@ -1,25 +1,5 @@
-import { lines } from 'mrm-core'
-
-const html = String.raw
+import { copyAllFiles } from '../util'
 
 module.exports = () => {
-    lines('index.html')
-        .set([
-            html`
-                <!DOCTYPE html>
-                <html>
-                    <head>
-                        <meta charset="UTF-8" />
-                        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no, viewport-fit=cover" />
-                        <title>%VITE_NAME%</title>
-                    </head>
-
-                    <body>
-                        <div id="root"></div>
-                        <script src="./src/index.tsx" type="module"></script>
-                    </body>
-                </html>
-            `,
-        ])
-        .save()
+    copyAllFiles(__dirname)
 }
---
+++ util.ts
@@ -0,0 +1,13 @@
+import { oneOf } from '@zardoy/utils'
+import fs from 'fs'
+import { lines } from 'mrm-core'
+import { join } from 'path'
+
+export const copyAllFiles = (__dirname: string) => {
+    const files = fs.readdirSync(__dirname).filter(name => !fs.lstatSync(join(__dirname, name)).isDirectory() && !oneOf(name, 'index.ts', 'index.js'))
+    for (const file of files) {
+        lines(file)
+            .set([fs.readFileSync(join(__dirname, file), 'utf-8')])
+            .save()
+    }
+}
