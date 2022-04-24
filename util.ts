c4f4540b2fca57185726e4d2d796ed3c442ac589
Vitaly Turovsky <vital2580@icloud.com>
2022-04-24 04:59:38 +0300

    add missing util and deps

--- util.ts
+++ util.ts
@@ -2,17 +2,34 @@ import { oneOf } from '@zardoy/utils'
 import fs from 'fs'
 import { lines } from 'mrm-core'
 import { join } from 'path'
+import { readPackageJsonFile } from 'typed-jsonfile'
 
-export const copyAllFiles = (__dirname: string) => {
-    const files = fs.readdirSync(__dirname).filter(name => !fs.lstatSync(join(__dirname, name)).isDirectory() && !oneOf(name, 'index.ts', 'index.js'))
+export const copyAllFiles = (__dirname: string, patchFiles: Record<string, (contents: string) => string | undefined> = {}, copySet: string[] | null = null) => {
+    const files =
+        copySet ?? fs.readdirSync(__dirname).filter(name => !fs.lstatSync(join(__dirname, name)).isDirectory() && !oneOf(name, 'index.ts', 'index.js'))
     for (const file of files) {
-        lines(file)
-            .set([fs.readFileSync(join(__dirname, file), 'utf-8')])
-            .save()
+        const patchFn = patchFiles[file] ?? (contents => contents)
+        const contents = patchFn(fs.readFileSync(join(__dirname, file), 'utf-8'))
+        if (contents === undefined) continue
+        lines(file).set([contents]).save()
     }
 }
 
 export const ensureLicense = () => {
     if (fs.existsSync('LICENSE') || fs.existsSync('LICENSE.md')) return
     require('./license/index')()
+}
+
+export const ensureGitignore = () => {
+    if (fs.existsSync('.gitignore')) return
+    require('./gitignore/index')()
+}
+
+export const hasVscodeFramework = async () => {
+    const packageJson = await readPackageJsonFile({ dir: '.' }).catch(() => null)
+    if (packageJson) {
+        if (Object.keys({ ...packageJson.dependencies, ...packageJson.devDependencies }).includes('vscode-framework')) {
+            return true
+        }
+    }
 }
