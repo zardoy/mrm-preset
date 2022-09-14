70912cc72865fff4d2cb3a50bf847d579b961bfd
Vitaly Turovsky <vital2580@icloud.com>
2022-09-14 14:28:00 +0300

    feat: update vscode & vscode-tested presets in action!

--- action/index.ts
+++ action/index.ts
@@ -1,16 +1,22 @@
 import fsExtra from 'fs-extra'
-import { lines } from 'mrm-core'
+import { install, lines } from 'mrm-core'
 import { join } from 'path'
+import { copyAllFiles } from '../util'
 
 module.exports = ({ preset }) => {
     const githubWorkflows = '.github/workflows'
     fsExtra.ensureDirSync(githubWorkflows)
 
-    const files = fsExtra.readdirSync(join(__dirname, preset))
+    const files = fsExtra.readdirSync(join(__dirname, preset)).filter(file => file.endsWith('.yml'))
     for (const file of files) {
         lines(join(githubWorkflows, file))
             .set([fsExtra.readFileSync(join(__dirname, preset, file), 'utf-8')])
             .save()
+    }
+
+    if (preset === 'vscode-tested') {
+        install(['chokidar-cli', '@vscode/test-electron', 'jest', '@types/jest', 'jest-environment-node', 'cross-env'], { pnpm: true })
+        copyAllFiles(__dirname, undefined, ['jest.e2e.config.js'])
     }
 }
 
